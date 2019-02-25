cc8666efd86cfb2b6d8f09b3045219180a9d5966
msumeet <msumeetiw@gmail.com>
2019-02-25 15:47:47 +0545

    Updates

---
+++ precache-manifest.ca9e0b0497e47b826406c58ce48350e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/citest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1a35d98432cadd2ae75",
+    "url": "/citest/static/js/runtime~main.b1a35d98.js"
+  },
+  {
+    "revision": "da40bcbeccfce490668d",
+    "url": "/citest/static/js/main.da40bcbe.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/citest/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "da40bcbeccfce490668d",
+    "url": "/citest/static/css/main.1542a6b7.chunk.css"
+  },
+  {
+    "revision": "b86f9e817fc795307acf9d67895e4ca0",
+    "url": "/citest/index.html"
+  }
+];
